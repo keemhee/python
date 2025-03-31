d5959f2e44b954de04f14f399740320c01627883
keemhee <eirmonar@gmail.com>
2025-03-31 17:34:18 +0900

    cat_threshold.py 업데이트
    'cat.jpg' 이미지 그레이스케일로 읽어온 후, 이진화 처리를 통해 임계값보다 큰 픽셀을 흰색(255)으로, 작은 픽셀을 검은색(0)으로 변환. 'q' 키를 누를 때까지 원본 이미지와 이진화된 이미지 화면에 표시

--- cv2_lessons/cat_threshold.py
+++ cv2_lessons/cat_threshold.py
@@ -1,15 +1,15 @@
-import cv2
+import cv2  # OpenCV 라이브러리를 임포트
 
-file = 'cat.jpg'
-img_gray = cv2.imread(file, cv2.IMREAD_GRAYSCALE) 
+file = 'cat.jpg'  # 처리할 이미지 파일 이름
+img_gray = cv2.imread(file, cv2.IMREAD_GRAYSCALE)  # 이미지를 그레이스케일로 읽기
 
-while True: 
-    _, img_thresh =cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY) 
+while True:  # 무한 루프
+    _, img_thresh = cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY)  # 이진화 처리 (임계값 150, 최대값 255, 이진화 방법)
 
-    cv2.imshow("Cat", img_gray)
-    cv2.imshow("Threshold", img_thresh)
+    cv2.imshow("Cat", img_gray)  # 원본 그레이스케일 이미지 표시
+    cv2.imshow("Threshold", img_thresh)  # 이진화된 이미지 표시
     
-    if cv2.waitKey(5) & 0xff == ord('q'):
-        break      
+    if cv2.waitKey(5) & 0xff == ord('q'):  # 'q' 키를 누르면 루프 종료
+        break
 
-cv2.destroyAllWindows()
+cv2.destroyAllWindows()  # 모든 OpenCV 창 닫기
